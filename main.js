a57e385fd9e6626257a2a88a81f32ca7e2feb223
ankitku3101 <ankit.ku3101@gmail.com>
2024-03-06 19:17:06 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,61 @@
+import './style.css';
+import gsap from 'gsap';
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Hide the preloader when the page is fully loaded
+  gsap.to("#preloader", { duration: 1, opacity: 0, onComplete: removePreloader });
+});
+
+// Function to remove the preloader from the DOM
+function removePreloader() {
+  document.getElementById("preloader").style.display = "none";
+}
+
+gsap.from(".clip-top, .clip-bottom", {
+  duration: 2,
+  delay: 1,
+  height: "50vh",
+  ease: "power4.inOut"
+});
+
+gsap.to(".marquee", {
+  duration: 3.5,
+  delay: 0.75,
+  top: "50%",
+  ease: "power4.inOut"
+});
+
+gsap.to([".clip-top .marquee", ".clip-bottom .marquee"], {
+  duration: 5,
+  delay: 1,
+  left: "100%",
+  ease: "power3.inOut"
+});
+
+gsap.to(".clip-center .marquee", {
+  duration: 5,
+  delay: 1,
+  left: "-10%",
+  ease: "power3.inOut"
+});
+
+gsap.to(".clip-top", {
+  duration: 2,
+  delay: 6,
+  clipPath: "inset(0 0 100% 0)",
+  ease: "power4.inOut"
+});
+
+gsap.to(".clip-bottom", {
+  duration: 2,
+  delay: 6,
+  clipPath: "inset(100% 0 0 0)",
+  ease: "power4.inOut"
+});
+
+gsap.to([".clip-top .marquee", ".clip-bottom .marquee", ".clip-center .marquee span"], {
+  duration: 1,
+  delay: 6,
+  opacity: 0,
+  ease: "power4.inOut"
+});
